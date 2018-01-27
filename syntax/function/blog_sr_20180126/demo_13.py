401b75ec0f784febdff724ab78faa1068e845362
贾明盛 <jms1209@qq.com>
2018-01-27 20:53:23 +0800

    整理格式

--- syntax/function/blog_sr_20180126/demo_13.py
+++ syntax/function/blog_sr_20180126/demo_13.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-# for-loops will use the scope they exist in and leave their defined loop-variable behind
+# for-loops will use the 01_scope they exist in and leave their defined loop-variable behind
 
 for a in range(5):
     if a == 4:
